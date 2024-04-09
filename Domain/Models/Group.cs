cc9789cab35435f48949663d7e824651b1303b8b
unknown <aykhanaa@code.edu.az>
2024-04-09 18:20:25 +0400

    create Domain layer

---
+++ Domain/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Models
+{
+    public class Group:BaseEntity
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int EducationId {  get; set; }
+        public Education Education { get; set; }
+    }
+}
